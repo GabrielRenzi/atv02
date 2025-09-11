71e91bc250945870f85fc3724533f1a91f5ad10e
Gabriel <gabriel.renzi@unidavi.edu.br>
2025-09-11 19:12:02 -0300

    meupwa

--- menu.js
+++ menu.js
@@ -1,19 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle');
     const navMobile = document.querySelector('.nav-mobile');
-    const menuIcon = document.getElementById('menu-icon');
-    
-    const openIconSrc = 'images/menu-icon.svg'; 
-    const closeIconSrc = 'images/close-icon.svg'; 
 
     if (navToggle) {
         navToggle.addEventListener('click', () => {
             const isVisible = navMobile.style.display === 'block';
             navMobile.style.display = isVisible ? 'none' : 'block';
-            
-            if (menuIcon) {
-                menuIcon.src = isVisible ? openIconSrc : closeIconSrc;
-            }
         });
     }
 });
